12f4e43149ce0e127f32c12ed68fa445a5bcb9af
AjayShukla007 <shuklaajay0838@gmail.com>
2024-01-01 13:08:05 +0530

    day16

---
+++ day16.js
@@ -0,0 +1,15 @@
+// Simple approach but did not work om every test cases
+// function searchRange(nums, target) {
+//   const tempNum = [];
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] === target) {
+//       tempNum.push(i);
+//     }
+//   }
+//   return tempNum.length>0?tempNum:[-1,-1];
+// }
+function searchRange(nums, target) {
+  let first = -1, last = -1;
+  
+}
+console.log(searchRange([5,7,7,8,8,10],8));
