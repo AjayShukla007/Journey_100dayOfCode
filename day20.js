f56b56dcb0c68620b8440a3c8c167d8c4b42cf3b
AjayShukla007 <shuklaajay0838@gmail.com>
2024-01-05 12:41:12 +0530

    day20

--- day20.js
+++ day20.js
@@ -1,7 +1,20 @@
 function rotation(arr, index) {
-  const reversion = arr.splice(arr.length-index,index);
+  const reversion = arr.splice(0,index).reverse();
   console.log(...reversion);
-  arr.reverse().push(...reversion);
+  console.log(...arr);
+  arr.unshift(...reversion);
+}
+
+// THIS WORKS PERFECTLY BUT SOMEHOW THE LEETCODE NOT ACCEPTING IT THAT THIS PROBLEM CAN BE SOPVED USING DIFFERENT METHODS AS WELL FUCK LEETCODE, 
+// I HAD TO USE TO SECOND METHOD WHICH I STOLE FROM ANOTHER DEV HEHE..
+function rotation2(arr, from, to) {
+  while(from < to){
+    let tempt = arr[from];
+    arr[from] = arr[to];
+    arr[to] = tempt;
+    from++;
+    to--;
+  }
 }
 function reverseKGroup(head, k) {
   // function reversion(arr, start, end){
@@ -11,11 +24,13 @@ function reverseKGroup(head, k) {
   //     start++;
   //     end--;
   // }
-    rotation(head, k);
     let temp = 0;
-    if (k < head.length) {
-    // k+=k;
-    // reverseKGroup(head, k);
+    rotation2(head, temp, k-1);
+    
+    k+=k;
+    if (k <= head.length) {
+    temp+=k;
+    rotation2(head, temp, k-1);
     }
     
   // for (let i = k; i < head.length; i += k) {
@@ -24,3 +39,42 @@ function reverseKGroup(head, k) {
 }
 //head = [1,2,3,4,5], k = 3, Output: [3,2,1,4,5]
 console.log(reverseKGroup([1, 2, 3, 4, 5], 2));
+console.log(reverseKGroup([1, 2, 3, 4, 5], 3));
+
+
+const reverseKGroup2 = function(head, k) {
+    let arr = [], store = head;
+    while(head){
+        arr.push(head.val);
+        head = head.next;
+    }
+    if(arr.length <= 1) return store;
+    
+    let ans = [], start = 0, end = k;
+
+    while(1){
+        if(end > arr.length){
+            for(let i=start;i<arr.length;i++){
+                ans.push(arr[i]);
+            }
+            break;
+        }
+        console.log(start, end)
+        const revArr  = [...arr].splice(start,k).reverse();
+        ans.push(...revArr)
+        start = end;
+        end = end + k;
+    }
+
+    head = store;
+    for(let i=0;i<ans.length;i++){
+        if(store){
+            store.val = ans[i];
+            store = store.next;
+        }
+    }
+
+    return head;
+};
+console.log(reverseKGroup2([1, 2, 3, 4, 5], 2));
+console.log(reverseKGroup2([1, 2, 3, 4, 5], 3));
