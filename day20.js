4086eb219c69de1c22e200c466e5da08e5e0ecea
AjayShukla007 <shuklaajay0838@gmail.com>
2024-01-05 12:13:06 +0530

    day20

--- day20.js
+++ day20.js
@@ -1,5 +1,6 @@
 function rotation(arr, index) {
-  const reversion = arr.splice(index);
+  const reversion = arr.splice(arr.length-index,index);
+  console.log(...reversion);
   arr.reverse().push(...reversion);
 }
 function reverseKGroup(head, k) {
@@ -11,16 +12,15 @@ function reverseKGroup(head, k) {
   //     end--;
   // }
     rotation(head, k);
+    let temp = 0;
     if (k < head.length) {
-    k+=k;
-    reverseKGroup(head, k);
+    // k+=k;
+    // reverseKGroup(head, k);
     }
     
   // for (let i = k; i < head.length; i += k) {
   // }
-  if (k > head.length) {
     return head;
-  }
 }
 //head = [1,2,3,4,5], k = 3, Output: [3,2,1,4,5]
 console.log(reverseKGroup([1, 2, 3, 4, 5], 2));
