2c017e174c40b2938d80712028b2abedf0e27aa1
AjayShukla007 <shuklaajay0838@gmail.com>
2024-01-11 12:16:01 +0530

    day26

--- day26.js
+++ day26.js
@@ -1,3 +1,9 @@
 function minimumSumTotal(triangle) {
-  
+  for (let row = triangle.length - 2; row > -1; row--)
+    for (let col = 0; col < triangle[row].length; col++)
+      triangle[row][col] += Math.min(
+        triangle[-~row][col],
+        triangle[-~row][-~col]
+      );
+  return triangle[0][0];
 }
