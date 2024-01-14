97d4122702566f1198af4108caa3924aabc448bc
AjayShukla007 <shuklaajay0838@gmail.com>
2024-01-14 13:41:59 +0530

    day29

---
+++ day29.js
@@ -0,0 +1,16 @@
+var findMin = function(nums) {
+  var left = 0;
+  var right = nums.length - 1;
+  var mid = 0;
+  while (left < right) {
+    mid = Math.floor((left + right) / 2);
+    if (nums[mid] > nums[right]) {
+      left = mid + 1;
+    } else if (nums[mid] < nums[right]) {
+      right = mid;
+    } else {
+      right--;
+    }
+  }
+  return nums[left];
+};
