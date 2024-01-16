4a1634e0adbf308c06ab6beccc8d50376a5e06f1
AjayShukla007 <shuklaajay0838@gmail.com>
2024-01-16 14:39:17 +0530

    day31

---
+++ day31.js
@@ -0,0 +1,28 @@
+var merge = function(intervals) {
+  var len = intervals.length;
+  var res = [];
+  var a = null;
+  var b = null;
+
+  intervals.sort((c, d) => (c.start - d.start));
+
+  for (var i = 0; i < len; i++) {
+    a = res[res.length - 1];
+    b = intervals[i];
+    if (overlap(a, b)) {
+      a.start = Math.min(a.start, b.start);
+      a.end = Math.max(a.end, b.end);
+    } else {
+      res.push(new Interval(b.start, b.end));
+    }
+  }
+
+  return res;
+};
+
+var overlap = function (a, b) {
+  if (!a || !b) return false;
+  if (b.start <= a.end && a.end <= b.end) return true;
+  if (a.start <= b.end && b.end <= a.end) return true;
+  return false;
+};
