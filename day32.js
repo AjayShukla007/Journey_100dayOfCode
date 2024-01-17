676031910426d2e046cc99a9bf57f256c7d937f0
AjayShukla007 <shuklaajay0838@gmail.com>
2024-01-17 20:51:15 +0530

    day32

---
+++ day32.js
@@ -0,0 +1,17 @@
+
+var combinationSum = function (candidates, target) {
+    var buffer = [];
+    var result = [];
+    search(0, target);
+    return result;
+
+    function search(startIdx, target) {
+        if (target === 0) return result.push(buffer.slice());
+        if (target < 0) return;
+        if (startIdx === candidates.length) return;
+        buffer.push(candidates[startIdx]);
+        search(startIdx, target - candidates[startIdx]);
+        buffer.pop();
+        search(startIdx + 1, target);
+    }
+};
