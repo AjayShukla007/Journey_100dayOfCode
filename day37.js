f8e6f8cbd0c22fca196516cd7c5e3f3cc4262adf
AjayShukla007 <shuklaajay0838@gmail.com>
2024-01-22 10:21:44 +0530

    day37

--- day37.js
+++ day37.js
@@ -1,3 +1,5 @@
+// we can use XOR ^
+// XOR performs a Boolean exclusive OR operation on each bit of its integer arguments and by looking at the XOR truth table above: One or the other is true, but not both. Therefore, if all but one value in the array is unique, eventually, each duplicate value will cancel each other out during the bitwise operations and we will end up with a unique non-duplicate value.
 
 const singleNum = (nums, target)=>{
   if(nums.length === 1) return nums[0];
