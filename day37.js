26cbc0b28c15b16ae4b2d2904db94e1005a2150a
AjayShukla007 <shuklaajay0838@gmail.com>
2024-01-22 10:20:36 +0530

    day37

--- day37.js
+++ day37.js
@@ -1,5 +1,10 @@
+
 const singleNum = (nums, target)=>{
   if(nums.length === 1) return nums[0];
   let result = 0;
-  nums.forEach(())
+  nums.forEach((e)=>{
+    result = result ^ e;
+  });
+  return result;
 };
+console.log(singleNum([1,1,2,2,4]));
