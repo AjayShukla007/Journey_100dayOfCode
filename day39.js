21d6222536d49b856a6c594942dce9f76c84e5bd
AjayShukla007 <shuklaajay0838@gmail.com>
2024-01-24 14:27:44 +0530

    day39

--- day39.js
+++ day39.js
@@ -19,3 +19,17 @@ var helper = function (nums, res, arr, start) {
     arr.pop();
   }
 };
+
+const subsetsOptimal = nums => {
+  function findSubset(array, subset) {
+        result.push([...subset]);
+        for(let i = 0; i < array.length; i++) {
+            subset.push(array[i]);
+            findSubset(array.slice(i + 1), subset);
+            subset.pop();
+        }
+    }
+    let result = [];
+    findSubset(nums, []);
+    return result;
+};
