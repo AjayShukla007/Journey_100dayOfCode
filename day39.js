ee2cd2c04618d86adde62fc8719c2edc12bf877b
AjayShukla007 <shuklaajay0838@gmail.com>
2024-01-24 14:19:30 +0530

    day39

--- day39.js
+++ day39.js
@@ -1,3 +1,19 @@
 const subsets = (nums)=>{
-  
+  var res = [];
+  helper(nums, res, [], 0);
+  return res;
+};
+
+var helper = function (nums, res, arr, start) {
+  var len = nums.length;
+
+  res.push(Array.from(arr));
+
+  if (start === len) return;
+
+  for (var i = start; i < len; i++) {
+    arr.push(nums[i]);
+    helper(nums, res, arr, i + 1);
+    arr.pop();
+  }
 }
