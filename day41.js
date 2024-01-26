57ef3ee5e83306b84d7f29426615a24acdde31eb
AjayShukla007 <shuklaajay0838@gmail.com>
2024-01-26 13:55:00 +0530

    day41

--- day41.js
+++ day41.js
@@ -1,13 +1,13 @@
-const moveZeroesOptimal = (nums) => {
-  let count = 0
-     nums.forEach((_, index)=> {
-		  if (nums[index] !== 0){
-			  nums[count] = nums[index]
-			  count++
-        }
-	})
-    for (let i = count; i < nums.length; i ++){
-		nums[i] = 0
+const moveZeroesOptimal = nums => {
+  let count = 0;
+  nums.forEach((_, index) => {
+    if (nums[index] !== 0) {
+      nums[count] = nums[index];
+      count++;
     }
-	return nums
-}
+  });
+  for (let i = count; i < nums.length; i++) {
+    nums[i] = 0;
+  }
+  return nums;
+};
