a304bd7af903c98f93dad7f5ee227217cdba638f
AjayShukla007 <shuklaajay0838@gmail.com>
2024-01-28 14:55:40 +0530

    day43

--- day43.js
+++ day43.js
@@ -1,1 +1,12 @@
-const getRow = rowInded => {};
+const getRow = rowInded => {
+  var res = [];
+  var i = 0;
+  var j = 0;
+  for (i = 0; i <= rowIndex; i++) {
+    res.unshift(1);
+    for (j = 1; j < i; j++) {
+      res[j] += res[j + 1];
+    }
+  }
+  return res;
+};
