3d4eed827969391edc39a11ce0940159f81ecdcc
AjayShukla007 <shuklaajay0838@gmail.com>
2024-01-29 15:29:16 +0530

    day44

--- day44.js
+++ day44.js
@@ -1,20 +1,19 @@
-
 const findRightInterval = function (intervals) {
-    let map = new Map();
-    let res = [];
-    let max = -Infinity;
-    for (i = 0; i < intervals.length; i++) {
-        map.set(intervals[i][0], i);
-        max = Math.max(intervals[i][0], max);
+  let map = new Map();
+  let res = [];
+  let max = -Infinity;
+  for (i = 0; i < intervals.length; i++) {
+    map.set(intervals[i][0], i);
+    max = Math.max(intervals[i][0], max);
+  }
+  for (i = 0; i < intervals.length; i++) {
+    if (intervals[i][1] > max) {
+      res.push(-1);
+    } else {
+      temp = intervals[i][1];
+      while (map.get(temp) === undefined) temp++;
+      res.push(map.get(temp));
     }
-    for (i = 0; i < intervals.length; i++) {
-        if (intervals[i][1] > max) {
-            res.push(-1);
-        } else {
-            temp = intervals[i][1];
-            while (map.get(temp) === undefined) temp++;
-            res.push(map.get(temp));
-        }
-    }
-    return res;
+  }
+  return res;
 };
