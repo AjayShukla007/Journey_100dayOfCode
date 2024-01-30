6f52626791212a66fd9c66498bf694b1f7a7b91c
AjayShukla007 <shuklaajay0838@gmail.com>
2024-01-30 17:52:46 +0530

    day45

--- day45.js
+++ day45.js
@@ -1,11 +1,10 @@
-const singleNumInSortedArray = (nums)=>{
+const singleNumInSortedArray = nums => {
   let left = 0;
   let right = nums.length - 1;
-  
-  while(left < right){
-    let middle = Math.round((left + right)/2);
-    if(m && 1) m--;
-    if (nums[m] === nums[m+1]) {
+  while (left < right) {
+    let m = Math.round((left + right) / 2);
+    if (m && 1) m--;
+    if (nums[m] === nums[m + 1]) {
       left = m + 2;
     } else {
       right = m;
@@ -13,3 +12,6 @@ const singleNumInSortedArray = (nums)=>{
   }
   return nums[left];
 };
+
+console.log(singleNumInSortedArray([1, 1, 2, 3, 3, 4, 4]));
+console.log(singleNumInSortedArray([1, 1, 2, 2, 3, 4, 4]));
