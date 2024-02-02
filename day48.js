5baa83cc87f6ef48c2121c7a36de4c7b8e2ba6e7
AjayShukla007 <shuklaajay0838@gmail.com>
2024-02-02 17:01:03 +0530

    day48

--- day48.js
+++ day48.js
@@ -1,3 +1,18 @@
 const findDublicate = nums => {
-  
+  let slow = 0;
+    let fast = 0;
+
+    do {
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+    } while (slow != fast);
+
+    fast = 0;
+
+    do {
+        slow = nums[slow];
+        fast = nums[fast];
+    } while (slow != fast);
+
+    return slow
 };
