f3580865cc1883f66e7d787370e42871a68292c5
AjayShukla007 <shuklaajay0838@gmail.com>
2024-02-02 17:01:12 +0530

    day48

--- day48.js
+++ day48.js
@@ -1,18 +1,18 @@
 const findDublicate = nums => {
   let slow = 0;
-    let fast = 0;
+  let fast = 0;
 
-    do {
-        slow = nums[slow];
-        fast = nums[nums[fast]];
-    } while (slow != fast);
+  do {
+    slow = nums[slow];
+    fast = nums[nums[fast]];
+  } while (slow != fast);
 
-    fast = 0;
+  fast = 0;
 
-    do {
-        slow = nums[slow];
-        fast = nums[fast];
-    } while (slow != fast);
+  do {
+    slow = nums[slow];
+    fast = nums[fast];
+  } while (slow != fast);
 
-    return slow
+  return slow;
 };
