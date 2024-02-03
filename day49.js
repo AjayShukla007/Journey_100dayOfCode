8a0fde60c53afc7b7208acc7b745b4aa87ee27ec
AjayShukla007 <shuklaajay0838@gmail.com>
2024-02-03 12:17:36 +0530

    day49

--- day49.js
+++ day49.js
@@ -1,3 +1,22 @@
 const hammingDistance = (x, y)=>{
-  
+  x = x.toString(2);
+    y = y.toString(2);
+    // Ensure that x and y have the same length in their binary representations
+    if (x.length < y.length) {
+        while (x.length !== y.length) {
+            x = "0" + x;
+        }
+    } else {
+        while (x.length !== y.length) {
+            y = "0" + y;
+        }
+    }
+    let distance = 0;
+    // Iterate through the binary representations and count differing bits
+    for (let i = 0; i < x.length; i++) {
+        if (x[i] !== y[i]) {
+            distance++;
+        }
+    }
+    return distance;
 }
