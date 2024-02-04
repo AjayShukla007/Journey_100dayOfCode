889b80e36c6af0457ca786bcf984ac4577148323
AjayShukla007 <shuklaajay0838@gmail.com>
2024-02-04 10:49:18 +0530

    day50

--- day50.js
+++ day50.js
@@ -1,14 +1,13 @@
 var grayCode = function (n) {
-    var res = [0];
-    helper(n, res, 0);
-    return res;
- 
+  var res = [0];
+  helper(n, res, 0);
+  return res;
 };
 var helper = function (n, res, num) {
-    if (num >= n) return;
-    var len = res.length;
-    for (var i = len - 1; i >= 0; i--) {
-        res.push(res[i] + len);
-    }
-    helper(n, res, num + 1);
+  if (num >= n) return;
+  var len = res.length;
+  for (var i = len - 1; i >= 0; i--) {
+    res.push(res[i] + len);
+  }
+  helper(n, res, num + 1);
 };
