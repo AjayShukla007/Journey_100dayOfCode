c320748ded44c9573dc2f76ef9c02b6227657961
AjayShukla007 <shuklaajay0838@gmail.com>
2024-02-05 19:34:32 +0530

    day51

--- day51.js
+++ day51.js
@@ -1,3 +1,12 @@
 const rangeLeftAndRight = (left, right)=>{
-  
+  let shiftcount = 0;
+
+    while(left != right && left > 0) {
+        shiftcount++;
+        left = left >> 1;
+        right = right >> 1;
+    }
+
+    return left << shiftcount;
+
 };
