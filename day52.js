2193f37e42751ac4fae43c43db8e4654644bb9cd
AjayShukla007 <shuklaajay0838@gmail.com>
2024-02-06 17:06:37 +0530

    day52

--- day52.js
+++ day52.js
@@ -1,19 +1,19 @@
 const singleNumberIII = nums => {
   let xorResult = 0;
-    let a = 0, b = 0, i = 0;
+  let a = 0,
+    b = 0,
+    i = 0;
 
-    for(i = 0; i < nums.length; i++){
-        xorResult ^= nums[i];
-    }
+  for (i = 0; i < nums.length; i++) {
+    xorResult ^= nums[i];
+  }
 
-    let setBitNo = xorResult & ~(xorResult - 1);
+  let setBitNo = xorResult & ~(xorResult - 1);
 
-    for(i = 0; i < nums.length; i++) {
-        if((nums[i] & setBitNo) === 0)
-            a ^= nums[i];
-        else
-            b ^= nums[i];
-    }
+  for (i = 0; i < nums.length; i++) {
+    if ((nums[i] & setBitNo) === 0) a ^= nums[i];
+    else b ^= nums[i];
+  }
 
-    return [a, b];
+  return [a, b];
 };
