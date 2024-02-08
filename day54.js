cd6d92322855cc635fe94e141519adfe630b1bc3
AjayShukla007 <shuklaajay0838@gmail.com>
2024-02-08 12:01:49 +0530

    day54

--- day54.js
+++ day54.js
@@ -1,3 +1,9 @@
 const sumOfTwoIntegers = (a, b) => {
-  
+  while (a !== 0 && b !== 0) {
+        const and = a & b;
+        const xor = a ^ b;
+        a = and << 1;
+        b = xor;
+    }
+    return a !== 0 ? a : b;
 };
