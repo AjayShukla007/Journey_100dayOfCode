b40af59407af778e6293c1ac0b2e2fffd99a0e39
AjayShukla007 <shuklaajay0838@gmail.com>
2024-02-12 22:02:18 +0530

    day58

--- day58.js
+++ day58.js
@@ -2,6 +2,10 @@ const findComplement = num => {
   let r = 0;
   let i = 1;
   while(num > i && i > 0){
-    
+    r += (num^i)&i;
+    i = (i<<1);
   }
+  return r;
 };
+
+console.log(findComplement(5));
