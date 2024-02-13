0551d66c8a06466b1bd3df7ad85a77b1dababe34
AjayShukla007 <shuklaajay0838@gmail.com>
2024-02-13 08:25:34 +0530

    day59

--- day59.js
+++ day59.js
@@ -1,3 +1,20 @@
 const bineryGap = n => {
-  
+  binary = N.toString(2);
+    binarysplit = binary.split('');
+    let count = 0;
+    let arr = [];
+    for (var i =0; i < binarysplit.length ; i++) {
+        if (binarysplit[i] === '1') {
+            count++;
+            arr.push(i);
+        }
+    }
+    if (count === 1) {
+        return 0;
+    }
+    let arr1 = [];
+    for (var j = 0 ; j < arr.length - 1; j++)  {
+        arr1[j] = arr[j + 1] - arr[j];
+    }
+return Math.max(...arr1);    
 };
