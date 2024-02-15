b0021be32a8787f95441591e8e343aec02703159
AjayShukla007 <shuklaajay0838@gmail.com>
2024-02-15 16:20:22 +0530

    day61

--- day61.js
+++ day61.js
@@ -1,3 +1,11 @@
 const totalHammingDistance = nums => {
-  
+  var tot=0;
+    for(var i=0; i<32; i++){
+        var count=0;
+        for(var j=0; j<nums.length; j++){
+            count+= (nums[j]>>i)&1;
+        }
+        tot+= count*(nums.length-count);
+    }
+    return tot;
 };
