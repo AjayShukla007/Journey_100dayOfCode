5f113b13470f4c135b43cb7b9b5df0838c3fd848
AjayShukla007 <shuklaajay0838@gmail.com>
2024-02-16 14:13:17 +0530

    day62

--- day62.js
+++ day62.js
@@ -1,3 +1,16 @@
 const toHex = n => {
+  let ret = ''
+  const chars = '0123456789abcdef'
   
+  if (num < 0) {
+    num += Math.pow(2, 32)
+  }
+ 
+  while (num > 0) {
+    const mod = num % 16
+    ret = chars.charAt(mod) + ret
+    num = Math.floor(num / 16)
+  }
+
+  return ret || '0'
 };
