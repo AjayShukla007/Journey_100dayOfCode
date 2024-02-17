c734ec221105e9bf71352391311956e5ee827f98
AjayShukla007 <shuklaajay0838@gmail.com>
2024-02-17 14:39:45 +0530

    day63

--- day63.js
+++ day63.js
@@ -1,8 +1,8 @@
 const decode = (encoded, first) => {
   const arr = new Array(encoded.length + 1);
-    arr[0] = first;
-    for (let i = 0; i < encoded.length; i++) {
-        arr[i+1] = encoded[i] ^ arr[i];
-    }
-    return arr;
+  arr[0] = first;
+  for (let i = 0; i < encoded.length; i++) {
+    arr[i + 1] = encoded[i] ^ arr[i];
+  }
+  return arr;
 };
