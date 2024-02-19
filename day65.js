15d3c8e4231f9abbc2994c08cc9ef70ea3d78011
AjayShukla007 <shuklaajay0838@gmail.com>
2024-02-19 22:30:06 +0530

    day65

--- day65.js
+++ day65.js
@@ -1,3 +1,12 @@
 const hasAltarnatingBits = n => {
-  
+  let output = true;
+    let cur, prev;
+    const bit = n.toString(2);
+    for (let i = 0; i < bit.length; i++) {
+        cur = bit[i];
+        prev = bit[i + 1];
+        cur !== prev ? output : output = false;
+    }
+    console.log(output);
+    return output
 };
