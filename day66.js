be51f5bdedffdb28cf1cfbc54bbbb3f2de3ff396
AjayShukla007 <shuklaajay0838@gmail.com>
2024-02-20 10:13:20 +0530

    day66

--- day66.js
+++ day66.js
@@ -1,10 +1,10 @@
 
 const bitwiseComplement = n => {
-  let mask = 1
-  while(mask<n){
-    mask=(mask << 1)+1
+  let mask = 1;
+  while (mask < n) {
+    mask = (mask << 1) + 1;
   }
-  return mask^n
+  return mask ^ n;
 };
 
 console.log(bitwiseComplement(5));
