c8ed050465373928d7753081037f10bc15e8a24a
AjayShukla007 <shuklaajay0838@gmail.com>
2024-02-22 16:20:55 +0530

    day68

--- day68.js
+++ day68.js
@@ -1,18 +1,28 @@
-class Solution {
-  public int minFlips(int a, int b, int c) {
-    final int kMaxBit = 30;
-    int ans = 0;
+// class Solution {
+//   public int minFlips(int a, int b, int c) {
+//     final int kMaxBit = 30;
+//     int ans = 0;
 
-    for (int i = 0; i < kMaxBit; ++i)
-      if ((c >> i & 1) == 1)
-        ans += ((a >> i & 1) == 0 && (b >> i & 1) == 0) ? 1 : 0;
-      else // (c >> i & 1) == 0
-        ans += ((a >> i & 1) == 1 ? 1 : 0) + ((b >> i & 1) == 1 ? 1 : 0);
+//     for (int i = 0; i < kMaxBit; ++i)
+//       if ((c >> i & 1) == 1)
+//         ans += ((a >> i & 1) == 0 && (b >> i & 1) == 0) ? 1 : 0;
+//       else // (c >> i & 1) == 0
+//         ans += ((a >> i & 1) == 1 ? 1 : 0) + ((b >> i & 1) == 1 ? 1 : 0);
 
-    return ans;
-  }
-}
+//     return ans;
+//   }
+// }
 
 const minFlips = (a, b, c) =>{
-  
-}
+  let kMaxBit = 30;
+  let ans = 0;
+  for (let i = 0; i < kMaxBit; i++) {
+    if((c >> i & 1) == 1){
+      ans+=((a >> i & 1) == 0 && (b >> i & 1) == 0) ? 1 : 0;
+    }else if((c >> i & 1) == 1){
+      ans += ((a >> i & 1) == 1 ? 1 : 0) + ((b >> i & 1) == 1 ? 1 : 0);
+
+    }
+  }
+  return ans;
+};
