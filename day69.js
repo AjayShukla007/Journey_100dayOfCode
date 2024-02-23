4d7f54a2af3a4a79969f02ff3b2ca241a46d42e8
AjayShukla007 <shuklaajay0838@gmail.com>
2024-02-23 19:55:59 +0530

    day69

---
+++ day69.js
@@ -0,0 +1,30 @@
+// A Simple Javascript program to compute
+// sum of bitwise OR of all pairs
+ 
+// Returns sum of bitwise OR
+// of all pairs
+const pairORSum = (arr, n) => {
+
+    let ans = 0; // Initialize result
+ 
+
+    // Consider all pairs (arr[i], arr[j) such that
+
+    // i < j
+
+    for (let i = 0; i < n; i++)
+
+        for (let j = i + 1; j < n; j++)
+
+            ans += arr[i] ^ arr[j];
+ 
+
+    return ans;
+}
+ 
+// Driver program to test above function
+let arr = [5, 9, 7, 6];
+let n = arr.length;
+document.write(pairORSum(arr, n));
+ 
+//
