aa0a5492004e78ebe7b93199d4ea559b956fe45d
AjayShukla007 <shuklaajay0838@gmail.com>
2024-02-24 22:12:19 +0530

    day70

--- day70.js
+++ day70.js
@@ -1,8 +1,8 @@
 const xorOparation = (n, start) => {
   let result = 0;
 
-    for(let i=0; i<n; i++) {
-        result^=(start+(2*i));
-    }
-    return result;
+  for (let i = 0; i < n; i++) {
+    result ^= start + 2 * i;
+  }
+  return result;
 };
