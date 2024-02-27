aa9fd668edbf2bc263cbbe033a297cdcbb124187
AjayShukla007 <shuklaajay0838@gmail.com>
2024-02-27 22:46:24 +0530

    day73

--- day73.js
+++ day73.js
@@ -1,3 +1,18 @@
+// int[] ans = new int[pref.length];
+
+//     ans[0] = pref[0];
+//     for (int i = 1; i < ans.length; ++i)
+//       ans[i] = pref[i] ^ pref[i - 1];
+
+//     return ans;
+
 const findArray = pref => {
-  
+  let ans = new Arrat(pref.length);
+  ans[0] = pref[0];
+  for (let i = 1; i < ans.length; ++i)
+      ans[i] = pref[i] ^ pref[i - 1];
+
+    return ans;
 };
+
+console.log(findArray());
