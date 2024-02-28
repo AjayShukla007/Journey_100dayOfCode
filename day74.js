be367c01986bb1b1dea12951f8670351e316a64c
AjayShukla007 <shuklaajay0838@gmail.com>
2024-02-28 10:10:20 +0530

    day74

--- day74.js
+++ day74.js
@@ -1,1 +1,17 @@
 // starting here
+class TreeNode {
+  constructor(val) {
+    this.val = val;
+    this.left = this.right = null;
+  }
+}
+
+function findBottomLeftValue(root) {
+  
+}
+
+// Example usage:
+// const root = new TreeNode(2);
+// root.left = new TreeNode(1);
+// root.right = new TreeNode(3);
+// console.log(findBottomLeftValue(root));
