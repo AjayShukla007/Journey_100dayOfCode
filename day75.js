5ca89918e089f1f1ec72c1a5e5ffce03508ddd56
AjayShukla007 <shuklaajay0838@gmail.com>
2024-02-29 21:06:02 +0530

    day75

--- day75.js
+++ day75.js
@@ -1,3 +1,19 @@
-const pow = (x, n) => {
-  
-};
+function pow(x, n) {
+  if (n === 0) {
+    return 1;
+  } else if (n < 0) {
+    x = 1 / x;
+    n = -n;
+  }
+
+  if (n % 2 === 0) {
+    const halfPow = pow(x, n / 2);
+    return halfPow * halfPow;
+  } else {
+    return x * pow(x, n - 1);
+  }
+}
+
+// Example usage:
+// const result = pow(2, 3);
+// console.log(result);
