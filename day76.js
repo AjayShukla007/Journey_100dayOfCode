ec658031af1de6cbd5e791a6b1e0bcdc78414b6e
AjayShukla007 <shuklaajay0838@gmail.com>
2024-03-01 21:37:57 +0530

    day76

--- day76.js
+++ day76.js
@@ -1,7 +1,7 @@
 const hasTrailingZeros = nums => {
   let cnt = 0;
-    for (const x of nums) {
-        cnt += (x & 1) ^ 1;
-    }
-    return cnt >= 2;
+  for (const x of nums) {
+    cnt += (x & 1) ^ 1;
+  }
+  return cnt >= 2;
 };
