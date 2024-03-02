05e5d92cda0317cc61279c0148b1510c5df53ccd
AjayShukla007 <shuklaajay0838@gmail.com>
2024-03-02 18:28:03 +0530

    day77

--- day77.js
+++ day77.js
@@ -1,3 +1,11 @@
+function bitCount(num) {
+    let count = 0;
+    while (num !== 0) {
+      count += num & 1;
+      num >>>= 1;
+    }
+    return count;
+  }
 const minBitFlip = (start, goal) => {
-  
+  return bitCount(start ^ goal);
 };
