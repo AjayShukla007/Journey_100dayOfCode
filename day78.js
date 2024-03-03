4e9fc6c800f011d22394ade518d7c4af67fd2780
AjayShukla007 <shuklaajay0838@gmail.com>
2024-03-03 12:58:48 +0530

    day78

--- day78.js
+++ day78.js
@@ -1,12 +1,12 @@
 const minOperations = (nums, k) => {
   const xors = nums.reduce((a, b) => a ^ b, 0);
-    return this.bitCount(k ^ xors);
+  return this.bitCount(k ^ xors);
 };
 function bitCount(num) {
   let count = 0;
-    while (num !== 0) {
-      count += num & 1;
-      num >>>= 1;
-    }
-    return count;
+  while (num !== 0) {
+    count += num & 1;
+    num >>>= 1;
+  }
+  return count;
 }
