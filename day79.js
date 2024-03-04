82c0cc737a54e7332bd6d3826a14a9d679818cab
AjayShukla007 <shuklaajay0838@gmail.com>
2024-03-04 21:28:06 +0530

    day79

--- day79.js
+++ day79.js
@@ -1,3 +1,14 @@
 const findKOr = (nums, k) => {
-  
-};
+  const kMaxBit = 30;
+  let ans = 0;
+
+  for (let i = 0; i <= kMaxBit; ++i) {
+    const count = nums.filter(num => (num >> i & 1) === 1).length;
+
+    if (count >= k) {
+      ans += Math.pow(2, i);
+    }
+  }
+
+  return ans;
+}
