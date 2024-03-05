e2d8c448cea1a86214234160b0fa57544c26d3a5
AjayShukla007 <shuklaajay0838@gmail.com>
2024-03-05 16:29:47 +0530

    day80

--- day80.js
+++ day80.js
@@ -1,3 +1,12 @@
-const evenOddBits = n => {
-  
-};
+const evenOddBit = (n) => {
+  const ans = [0, 0];
+  let i = 0; // 0 := even, 1 := odd
+
+  while (n > 0) {
+    ans[i] += n & 1;
+    n >>= 1;
+    i ^= 1;
+  }
+
+  return ans;
+}
