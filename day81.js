f2eed8fda12938fe3aa18a34d90fb06020afc899
AjayShukla007 <shuklaajay0838@gmail.com>
2024-03-06 20:31:41 +0530

    day81

--- day81.js
+++ day81.js
@@ -1,3 +1,8 @@
 const concatenatedBinery = n => {
-  
+  let ans = 1, len = 0b100
+    for (let i = 2; i <= n; i++) {
+        if (i === len) len <<= 1
+        ans = (ans * len + i) % 1000000007
+    }
+    return ans
 };
