47c5e094b044ce8df1c1ce18d14a321c8e25843d
AjayShukla007 <shuklaajay0838@gmail.com>
2024-03-07 21:37:12 +0530

    day82

---
+++ day82.js
@@ -0,0 +1,15 @@
+
+
+
+function sumIndicesWithKSetBits(nums,k) {
+    
+}
+
+// function bitCount(n: number): number {
+//     let count = 0;
+//     while (n) {
+//         n &= n - 1;
+//         count++;
+//     }
+//     return count;
+// }
