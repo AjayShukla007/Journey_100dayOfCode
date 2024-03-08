0c9ba6855006b45a10775ec306f122962b993cd9
AjayShukla007 <shuklaajay0838@gmail.com>
2024-03-08 19:22:08 +0530

    day83

--- day83.js
+++ day83.js
@@ -1,3 +1,16 @@
 function xorQueries(arr, queries) {
-  
+  const ans = [];
+  const xors = new Array(arr.length + 1).fill(0);
+
+  for (let i = 0; i < arr.length; ++i)
+    xors[i + 1] = xors[i] ^ arr[i];
+
+  let i = 0;
+  for (const query of queries) {
+    const left = query[0];
+    const right = query[1];
+    ans[i++] = xors[left] ^ xors[right + 1];
+  }
+
+  return ans;
 }
