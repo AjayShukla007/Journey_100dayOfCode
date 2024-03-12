eb41dd667bae28848c1d4f80d870de47b925ed58
AjayShukla007 <shuklaajay0838@gmail.com>
2024-03-12 14:42:39 +0530

    day87

--- day87.js
+++ day87.js
@@ -1,3 +1,21 @@
 function smallestSubarrays(nums) {
-  
+  const n = nums.length;
+  const f = new Array(32).fill(-1);
+  const ans = new Array(n);
+
+  for (let i = n - 1; i >= 0; --i) {
+    let t = 1;
+
+    for (let j = 0; j < 32; ++j) {
+      if ((nums[i] >> j) & 1) {
+        f[j] = i;
+      } else if (f[j] !== -1) {
+        t = Math.max(t, f[j] - i + 1);
+      }
+    }
+
+    ans[i] = t;
+  }
+
+  return ans;
 }
