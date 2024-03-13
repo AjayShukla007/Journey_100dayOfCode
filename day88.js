0e78c395e3d63a1f6ead8da7859cc47c91aecd88
AjayShukla007 <shuklaajay0838@gmail.com>
2024-03-13 15:38:45 +0530

    day88

--- day88.js
+++ day88.js
@@ -1,3 +1,12 @@
 function numberOfSteps(num) {
-  
+  let steps = 0;
+  while (num > 0) {
+    if (num % 2 === 0) {
+      num /= 2;
+    } else {
+      num -= 1;
+    }
+    steps += 1;
+  }
+  return steps;
 }
