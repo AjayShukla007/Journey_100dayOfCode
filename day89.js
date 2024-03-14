ebb0628c6c105c459f0a9202c595f5cba5380872
AjayShukla007 <shuklaajay0838@gmail.com>
2024-03-14 19:29:22 +0530

    day89

--- day89.js
+++ day89.js
@@ -1,4 +1,17 @@
 function longestSubarray(nums) {
-  
+  const maxValue = Math.max(...nums);
+  let longestSubarrayLength = 0;
+  let currentSubarrayLength = 0;
+
+  nums.forEach(value => {
+    if (value === maxValue) {
+      currentSubarrayLength++;
+      longestSubarrayLength = Math.max(longestSubarrayLength, currentSubarrayLength);
+    } else {
+      currentSubarrayLength = 0;
+    }
+  });
+
+  return longestSubarrayLength;
 }
 
