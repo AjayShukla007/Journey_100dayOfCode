6df487bc2126521f83f992fbeae97916562cebca
AjayShukla007 <shuklaajay0838@gmail.com>
2024-03-15 22:46:26 +0530

    day90

--- day90.js
+++ day90.js
@@ -1,3 +1,9 @@
 const maxOperations = nums => {
-  
+  const s = nums[0] + nums[1];
+    const n = nums.length;
+    let ans = 0;
+    for (let i = 0; i + 1 < n && nums[i] + nums[i + 1] === s; i += 2) {
+        ++ans;
+    }
+    return ans;
 };
