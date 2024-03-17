7b11cc4d19a50a6dc6c3fe8a5e4eca0a3880c35f
AjayShukla007 <shuklaajay0838@gmail.com>
2024-03-17 17:47:57 +0530

    day92

--- day92.js
+++ day92.js
@@ -1,3 +1,9 @@
 function getWordsInLongestSubsequence(words, groups) {
-  
+  const ans = [];
+    for (let i = 0; i < n; ++i) {
+        if (i === 0 || groups[i] !== groups[i - 1]) {
+            ans.push(words[i]);
+        }
+    }
+    return ans;
 }
