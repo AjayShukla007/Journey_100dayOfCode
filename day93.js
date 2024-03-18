77c56f9ee77da105eec7e3fff28d6f9c717c485f
AjayShukla007 <shuklaajay0838@gmail.com>
2024-03-18 21:28:50 +0530

    day93

--- day93.js
+++ day93.js
@@ -1,15 +1,17 @@
 function lengthOfLongestSubsequence(nums, target) {
   const n = nums.length;
-    const f = Array.from({ length: n + 1 }, () => Array(target + 1).fill(-Infinity));
-    f[0][0] = 0;
-    for (let i = 1; i <= n; ++i) {
-        const x = nums[i - 1];
-        for (let j = 0; j <= target; ++j) {
-            f[i][j] = f[i - 1][j];
-            if (j >= x) {
-                f[i][j] = Math.max(f[i][j], f[i - 1][j - x] + 1);
-            }
-        }
+  const f = Array.from({ length: n + 1 }, () =>
+    Array(target + 1).fill(-Infinity)
+  );
+  f[0][0] = 0;
+  for (let i = 1; i <= n; ++i) {
+    const x = nums[i - 1];
+    for (let j = 0; j <= target; ++j) {
+      f[i][j] = f[i - 1][j];
+      if (j >= x) {
+        f[i][j] = Math.max(f[i][j], f[i - 1][j - x] + 1);
+      }
     }
-    return f[n][target] <= 0 ? -1 : f[n][target];
+  }
+  return f[n][target] <= 0 ? -1 : f[n][target];
 }
