930cf39e34d486f8dc7576a9ce09bc6c5e603842
AjayShukla007 <shuklaajay0838@gmail.com>
2024-03-22 23:37:15 +0530

    day97

--- day97.js
+++ day97.js
@@ -1,3 +1,10 @@
 const join = (arr1, arr2) => {
-  
+  const ob = {};
+    for(const i of arr1) {
+        ob[i.id] = i;
+    }
+    for(const i of arr2) {
+        ob[i.id] = ob[i.id] ? {...ob[i.id], ...i} : i;
+    }
+    return Object.values(ob);
 };
