de928f9f73b94472b08fd974e4ac9d16cadb7206
AjayShukla007 <shuklaajay0838@gmail.com>
2024-03-24 18:51:45 +0530

    day99

--- day99.js
+++ day99.js
@@ -1,3 +1,13 @@
 function findMatrix(nums) {
-  
+  let ans = [];
+  let count = new Array(nums.length + 1).fill(0);
+
+  for (const num of nums) {
+    if (++count[num] > ans.length) {
+      ans.push([]);
+    }
+    ans[count[num] - 1].push(num);
+  }
+
+  return ans;
 }
